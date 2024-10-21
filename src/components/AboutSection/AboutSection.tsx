26d2e83c2bfff87f5ef915babe68dcc36d18c57a
PranavNampally <63918027+PranavNampally@users.noreply.github.com>
2024-10-21 20:04:56 +0530

    about me template added

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -4,6 +4,13 @@ const AboutSection = () => {
   return (
     <div className='bg-gray-dark-50 px-4 py-16 flex flex-col gap-6'>
       <div className='bg-gray-dark-200 self-center px-[20px] py-1 rounded-xl'><span className='text-gray-dark-600'>About me</span></div>
+      <div className='mt-6 flex flex-col gap-12'>
+        <div className='h-[380px] bg-black'/> {/* Image */}
+        <div className='flex flex-col gap-4'>
+          <div className='text-gray-dark-900 font-semibold text-2xl'>heading</div>
+          <div className='text-gray-dark-600 text-base'>content block</div>
+        </div>
+      </div>
     </div>
   )
 }
