7c178bfeb4449168da0a7363152429a63384316e
PranavNampally <63918027+PranavNampally@users.noreply.github.com>
2025-05-17 17:07:57 +0530

    build error fixes

--- src/components/magicui/blur-fade.tsx
+++ src/components/magicui/blur-fade.tsx
@@ -2,7 +2,8 @@
 
 import { AnimatePresence, motion, useInView, Variants } from "framer-motion";
 import { useRef } from "react";
-
+type MarginValue = `${number}${'px' | '%'}`;
+type MarginType = MarginValue | `${MarginValue} ${MarginValue}` | `${MarginValue} ${MarginValue} ${MarginValue}` | `${MarginValue} ${MarginValue} ${MarginValue} ${MarginValue}`;
 interface BlurFadeProps {
   children: React.ReactNode;
   className?: string;
@@ -14,7 +15,7 @@ interface BlurFadeProps {
   delay?: number;
   yOffset?: number;
   inView?: boolean;
-  inViewMargin?: string;
+  inViewMargin?: MarginType;
   blur?: string;
 }
 const BlurFade = ({
