8ba189b1ce6fa26abee6b48353a112401660870c
PranavNampally <63918027+PranavNampally@users.noreply.github.com>
2025-05-18 02:08:56 +0530

    updating projects section

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -4,12 +4,12 @@ import { HomeIcon, NotebookIcon } from "lucide-react";
 export const DATA = {
   name: "Pranav Nampally",
   initials: "PN",
-  url: "https://pranav.io",
+  url: "https://pranavnampally.github.io/",
   location: "Bangalore, IND",
   locationLink: "https://www.google.co.in/maps/place/Bengaluru",
   description: `Software Developer skilled in React.js, Next.js, and TypeScript, with experience in creating high-impact web applications. Passionate about contributing to innovative projects that emphasize usability and scalability.`,
   summary:
-    "Hey!, I'm a frontend web developer currently working at ZEE Limited from past 1.5 year and have contributed to many innovative projects at my company. In the past, [I've completed my bachelor's in computer science and engineering from IIT Dharwad](/#education), interned at tech companies in Bangalore, and [competed in SIH in my final year](/#hackathons).",
+    `Hey!, I'm a frontend web developer currently working at ZEE Limited from past ${new Date().getFullYear() - 2023} years and have contributed to many innovative projects at my company. In the past, [I've completed my bachelor's in computer science and engineering from IIT Dharwad](/#education), interned at tech companies in Bangalore, and [competed in SIH in my final year](/#hackathons).`,
   avatarUrl: "/me.jpg",
   skills: [
     "React.js",
@@ -30,7 +30,7 @@ export const DATA = {
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
   ],
   contact: {
     email: "pranavnampally.work@gmail.com",
@@ -123,127 +123,36 @@ export const DATA = {
   ],
   projects: [
     {
-      title: "Chat Collect",
-      href: "https://chatcollect.com",
+      title: "WatchFlix",
+      href: "https://watch-flix-pranav-n.vercel.app/",
       dates: "Jan 2024 - Feb 2024",
       active: true,
       description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+        "",
       technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
+        "React.js",
+        "JavaScript",
+        "Material UI",
+        "Recoil",
+        "RTL"
       ],
       links: [
         {
           type: "Website",
-          href: "https://chatcollect.com",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    },
-    {
-      title: "Magic UI",
-      href: "https://magicui.design",
-      dates: "June 2023 - Present",
-      active: true,
-      description:
-        "Designed, developed and sold animated UI components for developers.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://magicui.design",
+          href: "https://watch-flix-pranav-n.vercel.app/",
           icon: <Icons.globe className="size-3" />,
         },
         {
           type: "Source",
-          href: "https://github.com/magicuidesign/magicui",
+          href: "https://github.com/PranavNampally/WatchFlix-PranavN",
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "",
-      video: "https://cdn.magicui.design/bento-grid.mp4",
-    },
-    {
-      title: "llm.report",
-      href: "https://llm.report",
-      dates: "April 2023 - September 2023",
-      active: true,
-      description:
-        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://llm.report",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/dillionverma/llm.report",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video: "https://cdn.llm.report/openai-demo.mp4",
-    },
-    {
-      title: "Automatic Chat",
-      href: "https://automatic.chat",
-      dates: "April 2023 - March 2024",
-      active: true,
-      description:
-        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://automatic.chat",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
+      image: "/watchflix.png",
       video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+        "",
     },
+
   ],
   hackathons: [
     {
